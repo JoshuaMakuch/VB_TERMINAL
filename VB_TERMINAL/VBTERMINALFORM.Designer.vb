82155f1813ab8e5404856656ddd7050116e1a111
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-29 09:18:30 -0600

    Done

--- VB_TERMINAL/VBTERMINALFORM.Designer.vb
+++ VB_TERMINAL/VBTERMINALFORM.Designer.vb
@@ -29,6 +29,16 @@ Partial Class VBTERMINALFORM
         Me.ScanButton = New System.Windows.Forms.Button()
         Me.PortOpen = New System.Windows.Forms.Button()
         Me.PortData = New System.Windows.Forms.ListBox()
+        Me.SelectBaud = New System.Windows.Forms.Button()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.DataTextBox = New System.Windows.Forms.TextBox()
+        Me.DataPacketTextBox = New System.Windows.Forms.Label()
+        Me.SendPacketButton = New System.Windows.Forms.Button()
+        Me.ClearPacketButton = New System.Windows.Forms.Button()
+        Me.OutTerm = New System.Windows.Forms.ListBox()
+        Me.InTerm = New System.Windows.Forms.ListBox()
+        Me.OutputClearButton = New System.Windows.Forms.Button()
+        Me.InputClearButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Timer1
@@ -40,7 +50,7 @@ Partial Class VBTERMINALFORM
         Me.PortSelect.ItemHeight = 16
         Me.PortSelect.Location = New System.Drawing.Point(178, 12)
         Me.PortSelect.Name = "PortSelect"
-        Me.PortSelect.Size = New System.Drawing.Size(203, 180)
+        Me.PortSelect.Size = New System.Drawing.Size(203, 244)
         Me.PortSelect.TabIndex = 0
         '
         'ScanButton
@@ -49,32 +59,130 @@ Partial Class VBTERMINALFORM
         Me.ScanButton.Name = "ScanButton"
         Me.ScanButton.Size = New System.Drawing.Size(160, 92)
         Me.ScanButton.TabIndex = 1
-        Me.ScanButton.Text = "Scan"
+        Me.ScanButton.Text = "Scan Ports"
         Me.ScanButton.UseVisualStyleBackColor = True
         '
         'PortOpen
         '
-        Me.PortOpen.Location = New System.Drawing.Point(12, 110)
+        Me.PortOpen.Location = New System.Drawing.Point(12, 208)
         Me.PortOpen.Name = "PortOpen"
         Me.PortOpen.Size = New System.Drawing.Size(160, 92)
         Me.PortOpen.TabIndex = 2
-        Me.PortOpen.Text = "Port Open"
+        Me.PortOpen.Text = "Connect"
         Me.PortOpen.UseVisualStyleBackColor = True
         '
         'PortData
         '
         Me.PortData.FormattingEnabled = True
         Me.PortData.ItemHeight = 16
-        Me.PortData.Location = New System.Drawing.Point(178, 198)
+        Me.PortData.Location = New System.Drawing.Point(178, 270)
         Me.PortData.Name = "PortData"
-        Me.PortData.Size = New System.Drawing.Size(203, 180)
+        Me.PortData.Size = New System.Drawing.Size(203, 260)
         Me.PortData.TabIndex = 3
+        '
+        'SelectBaud
+        '
+        Me.SelectBaud.Location = New System.Drawing.Point(12, 110)
+        Me.SelectBaud.Name = "SelectBaud"
+        Me.SelectBaud.Size = New System.Drawing.Size(160, 92)
+        Me.SelectBaud.TabIndex = 4
+        Me.SelectBaud.Text = "Select Baud"
+        Me.SelectBaud.UseVisualStyleBackColor = True
+        '
+        'QuitButton
+        '
+        Me.QuitButton.Location = New System.Drawing.Point(12, 430)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(160, 92)
+        Me.QuitButton.TabIndex = 5
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'DataTextBox
+        '
+        Me.DataTextBox.Location = New System.Drawing.Point(387, 34)
+        Me.DataTextBox.Name = "DataTextBox"
+        Me.DataTextBox.Size = New System.Drawing.Size(462, 22)
+        Me.DataTextBox.TabIndex = 6
+        '
+        'DataPacketTextBox
+        '
+        Me.DataPacketTextBox.AutoSize = True
+        Me.DataPacketTextBox.Location = New System.Drawing.Point(387, 15)
+        Me.DataPacketTextBox.Name = "DataPacketTextBox"
+        Me.DataPacketTextBox.Size = New System.Drawing.Size(81, 16)
+        Me.DataPacketTextBox.TabIndex = 7
+        Me.DataPacketTextBox.Text = "Data Packet"
+        '
+        'SendPacketButton
+        '
+        Me.SendPacketButton.Location = New System.Drawing.Point(390, 60)
+        Me.SendPacketButton.Name = "SendPacketButton"
+        Me.SendPacketButton.Size = New System.Drawing.Size(160, 44)
+        Me.SendPacketButton.TabIndex = 8
+        Me.SendPacketButton.Text = "Send Packet"
+        Me.SendPacketButton.UseVisualStyleBackColor = True
+        '
+        'ClearPacketButton
+        '
+        Me.ClearPacketButton.Location = New System.Drawing.Point(556, 60)
+        Me.ClearPacketButton.Name = "ClearPacketButton"
+        Me.ClearPacketButton.Size = New System.Drawing.Size(160, 44)
+        Me.ClearPacketButton.TabIndex = 9
+        Me.ClearPacketButton.Text = "Clear Packet"
+        Me.ClearPacketButton.UseVisualStyleBackColor = True
+        '
+        'OutTerm
+        '
+        Me.OutTerm.FormattingEnabled = True
+        Me.OutTerm.ItemHeight = 16
+        Me.OutTerm.Location = New System.Drawing.Point(387, 110)
+        Me.OutTerm.Name = "OutTerm"
+        Me.OutTerm.Size = New System.Drawing.Size(228, 372)
+        Me.OutTerm.TabIndex = 10
+        '
+        'InTerm
+        '
+        Me.InTerm.FormattingEnabled = True
+        Me.InTerm.ItemHeight = 16
+        Me.InTerm.Location = New System.Drawing.Point(621, 112)
+        Me.InTerm.Name = "InTerm"
+        Me.InTerm.Size = New System.Drawing.Size(228, 372)
+        Me.InTerm.TabIndex = 11
+        '
+        'OutputClearButton
+        '
+        Me.OutputClearButton.Location = New System.Drawing.Point(387, 486)
+        Me.OutputClearButton.Name = "OutputClearButton"
+        Me.OutputClearButton.Size = New System.Drawing.Size(160, 44)
+        Me.OutputClearButton.TabIndex = 12
+        Me.OutputClearButton.Text = "Clear"
+        Me.OutputClearButton.UseVisualStyleBackColor = True
+        '
+        'InputClearButton
+        '
+        Me.InputClearButton.Location = New System.Drawing.Point(621, 490)
+        Me.InputClearButton.Name = "InputClearButton"
+        Me.InputClearButton.Size = New System.Drawing.Size(160, 44)
+        Me.InputClearButton.TabIndex = 13
+        Me.InputClearButton.Text = "Clear"
+        Me.InputClearButton.UseVisualStyleBackColor = True
         '
         'VBTERMINALFORM
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(903, 534)
+        Me.Controls.Add(Me.InputClearButton)
+        Me.Controls.Add(Me.OutputClearButton)
+        Me.Controls.Add(Me.InTerm)
+        Me.Controls.Add(Me.OutTerm)
+        Me.Controls.Add(Me.ClearPacketButton)
+        Me.Controls.Add(Me.SendPacketButton)
+        Me.Controls.Add(Me.DataPacketTextBox)
+        Me.Controls.Add(Me.DataTextBox)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.SelectBaud)
         Me.Controls.Add(Me.PortData)
         Me.Controls.Add(Me.PortOpen)
         Me.Controls.Add(Me.ScanButton)
@@ -82,6 +190,7 @@ Partial Class VBTERMINALFORM
         Me.Name = "VBTERMINALFORM"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -91,4 +200,14 @@ Partial Class VBTERMINALFORM
     Friend WithEvents ScanButton As Button
     Friend WithEvents PortOpen As Button
     Friend WithEvents PortData As ListBox
+    Friend WithEvents SelectBaud As Button
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents DataTextBox As TextBox
+    Friend WithEvents DataPacketTextBox As Label
+    Friend WithEvents SendPacketButton As Button
+    Friend WithEvents ClearPacketButton As Button
+    Friend WithEvents OutTerm As ListBox
+    Friend WithEvents InTerm As ListBox
+    Friend WithEvents OutputClearButton As Button
+    Friend WithEvents InputClearButton As Button
 End Class
--- VB_TERMINAL/VBTERMINALFORM.vb
+++ VB_TERMINAL/VBTERMINALFORM.vb
@@ -4,9 +4,14 @@
 'VB Terminal Form
 'https://github.com/JoshuaMakuch/VB_TERMINAL
 
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar
+
 Public Class VBTERMINALFORM
 
     Dim PortState As Boolean
+    Public receiveByte(18) As Byte 'Receive Data Bytes
+
 
     Private Sub VBTERMINALFORM_UnLoad(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
@@ -58,8 +63,106 @@ Public Class VBTERMINALFORM
         End If
     End Sub
 
+    'Every 100mS it will update the PortData List box
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim DataIn As String
+        DataIn = ""
+        PortData.Items.Clear()
+        PortData.Items.Add("Com Port = " & SerialPort1.PortName) 'show current baud rate
+        PortData.Items.Add("Baud Rate = " & SerialPort1.BaudRate) 'show current baud rate
+        PortData.Items.Add("Data Bits = " & SerialPort1.DataBits)
+        PortData.Items.Add("Stop Bits = " & SerialPort1.StopBits)
+        PortData.Items.Add("Parity = " & SerialPort1.Parity)
+    End Sub
 
+    'This is what will set the port when the user clicks on something in the Port Select
+    Private Sub PortSelect_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PortSelect.SelectedIndexChanged
+        Try
+            SerialPort1.Close() 'try to close port before change
+        Catch ex As Exception
+
+        End Try
+        PortOpen.Text = "Connect"
+        PortState = False
+        Try
+            SerialPort1.BaudRate = PortSelect.SelectedItem 'see if Baud Rate data Is in the list box
+        Catch ex As Exception
+            SerialPort1.PortName = PortSelect.SelectedItem ' bot Baud Rate, save port Name
+        End Try
+    End Sub
+
+    'Handles when the select baud rate button is pressed
+    Private Sub SelectBaud_Click(sender As Object, e As EventArgs) Handles SelectBaud.Click
+        PortSelect.Items.Clear() 'Clear list Box and load Baud Rate values
+        PortSelect.Items.Add(1200)
+        PortSelect.Items.Add(2400)
+        PortSelect.Items.Add(4800)
+        PortSelect.Items.Add(9600)
+        PortSelect.Items.Add(19200)
+        PortSelect.Items.Add(38400)
+        PortSelect.Items.Add(57600)
+        PortSelect.Items.Add(115200)
+        PortSelect.Items.Add(230400)
+        PortSelect.Items.Add(460800)
+        PortSelect.Items.Add(921600)
+    End Sub
+
+    'Handles the quit button
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs) Handles QuitButton.Click
+        Try
+            SerialPort1.Close()
+            Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    'Handles send packet button
+    Private Sub SendPacketButton_Click(sender As Object, e As EventArgs) Handles SendPacketButton.Click
+        Timer1.Enabled = False 'stop timer
+        Dim DataOut 'Transmit Variable
+        Dim DataIn1, DataIn2, DataIn3, DataIn4 As Integer 'Receive Variables
+        DataOut = DataTextBox.Text 'Load transmit variable with information from text box
+        If PortState = True Then 'Test if port is open
+            If DataOut IsNot "" Then 'Test transmit data is not blank
+                SerialPort1.Write(DataOut, 0, 1) 'Send data
+                OutTerm.Items.Add(DataOut) 'Log sent data
+            Else 'Send Data was blank
+                Timer1.Enabled = True 'restart timer
+                Exit Sub 'Leave
+            End If
+            Try 'Attempt to receive
+                SerialPort1.Read(receiveByte, 0, 10) 'Read Serial Port
+                DataIn1 = receiveByte(0) 'Save Byte0
+                DataIn2 = receiveByte(1) 'Save Byte1
+                DataIn3 = receiveByte(2) 'Save Byte2
+                DataIn3 = receiveByte(3) 'Save Byte3
+                'Add data to input list box
+                InTerm.Items.Add(DataIn1 & vbTab & DataIn2 & vbTab & DataIn3 & vbTab & DataIn4)
+            Catch ex As Exception
+            End Try
+
+        Else
+            MsgBox("Please configure and open serial port to procede") 'Failure if port is not open
+        End If
+        Timer1.Enabled = True 'restart timer 
+
+
+    End Sub
+
+    'Clear Packet Button
+    Private Sub ClearPacketButton_Click(sender As Object, e As EventArgs) Handles ClearPacketButton.Click
+        DataTextBox.Text = ""
+    End Sub
+
+    'Handles output clear button click
+    Private Sub OutputClearButton_Click(sender As Object, e As EventArgs) Handles OutputClearButton.Click
+        OutTerm.Items.Clear()
+    End Sub
+
+    'Handles input clear button click
+    Private Sub InputClearButton_Click(sender As Object, e As EventArgs) Handles InputClearButton.Click
+        InTerm.Items.Clear()
     End Sub
 
 End Class
