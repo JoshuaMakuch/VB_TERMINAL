ab24fea0b1cb903ad64d22c5cdf60ccef313913d
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-29 08:47:31 -0600

    Added everything up to the timer1 tick

--- VB_TERMINAL/VBTERMINALFORM.Designer.vb
+++ VB_TERMINAL/VBTERMINALFORM.Designer.vb
@@ -25,13 +25,60 @@ Partial Class VBTERMINALFORM
         Me.components = New System.ComponentModel.Container()
         Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PortSelect = New System.Windows.Forms.ListBox()
+        Me.ScanButton = New System.Windows.Forms.Button()
+        Me.PortOpen = New System.Windows.Forms.Button()
+        Me.PortData = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'PortSelect
+        '
+        Me.PortSelect.FormattingEnabled = True
+        Me.PortSelect.ItemHeight = 16
+        Me.PortSelect.Location = New System.Drawing.Point(178, 12)
+        Me.PortSelect.Name = "PortSelect"
+        Me.PortSelect.Size = New System.Drawing.Size(203, 180)
+        Me.PortSelect.TabIndex = 0
+        '
+        'ScanButton
+        '
+        Me.ScanButton.Location = New System.Drawing.Point(12, 12)
+        Me.ScanButton.Name = "ScanButton"
+        Me.ScanButton.Size = New System.Drawing.Size(160, 92)
+        Me.ScanButton.TabIndex = 1
+        Me.ScanButton.Text = "Scan"
+        Me.ScanButton.UseVisualStyleBackColor = True
+        '
+        'PortOpen
+        '
+        Me.PortOpen.Location = New System.Drawing.Point(12, 110)
+        Me.PortOpen.Name = "PortOpen"
+        Me.PortOpen.Size = New System.Drawing.Size(160, 92)
+        Me.PortOpen.TabIndex = 2
+        Me.PortOpen.Text = "Port Open"
+        Me.PortOpen.UseVisualStyleBackColor = True
+        '
+        'PortData
+        '
+        Me.PortData.FormattingEnabled = True
+        Me.PortData.ItemHeight = 16
+        Me.PortData.Location = New System.Drawing.Point(178, 198)
+        Me.PortData.Name = "PortData"
+        Me.PortData.Size = New System.Drawing.Size(203, 180)
+        Me.PortData.TabIndex = 3
         '
         'VBTERMINALFORM
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(903, 534)
+        Me.Controls.Add(Me.PortData)
+        Me.Controls.Add(Me.PortOpen)
+        Me.Controls.Add(Me.ScanButton)
+        Me.Controls.Add(Me.PortSelect)
         Me.Name = "VBTERMINALFORM"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -40,4 +87,8 @@ Partial Class VBTERMINALFORM
 
     Friend WithEvents SerialPort1 As IO.Ports.SerialPort
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents PortSelect As ListBox
+    Friend WithEvents ScanButton As Button
+    Friend WithEvents PortOpen As Button
+    Friend WithEvents PortData As ListBox
 End Class
--- VB_TERMINAL/VBTERMINALFORM.vb
+++ VB_TERMINAL/VBTERMINALFORM.vb
@@ -1,8 +1,64 @@
-﻿
+﻿'Joshua Makuch
+'RCET 3371
+'September 29, 2023
+'VB Terminal Form
+'https://github.com/JoshuaMakuch/VB_TERMINAL
 
 Public Class VBTERMINALFORM
 
+    Dim PortState As Boolean
+
+    Private Sub VBTERMINALFORM_UnLoad(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            SerialPort1.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    'Initially Sets Serial Port information
     Private Sub VBTERMINALFORM_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PortSelect.Items.Clear()
+        PortState = False
+        SerialPort1.BaudRate = 9600 '9600 baud rate
+        SerialPort1.DataBits = 8 '8 data bits
+        SerialPort1.StopBits = IO.Ports.StopBits.One '1 stop bit
+        SerialPort1.Parity = IO.Ports.Parity.None 'no parity
+        Timer1.Enabled = True 'timer set to 50ms
+    End Sub
+
+    'Find and display all available com ports
+    Private Sub ScanButton_Click(sender As Object, e As EventArgs) Handles ScanButton.Click
+        PortSelect.Items.Clear() 'Clear current list
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            PortSelect.Items.Add(sp) 'add discovered port to listbox
+        Next
+    End Sub
+
+    'This opens a port and notifies the user if its wrong
+    Private Sub PortOpen_Click(sender As Object, e As EventArgs) Handles PortOpen.Click
+        If PortOpen.Text = "Connect" Then
+            Try
+                SerialPort1.Open()
+                PortOpen.Text = "Disconnect"
+                PortState = True
+            Catch ex As Exception
+                MsgBox("Port Already Open Or Port Unavailable")
+                PortOpen.Text = "Connect"
+                PortState = False
+            End Try
+        Else
+            Try
+                SerialPort1.Close()
+            Catch ex As Exception
+
+            End Try
+            PortState = False
+            PortOpen.Text = "Connect"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
 
     End Sub
 
